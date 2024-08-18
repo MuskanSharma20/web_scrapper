eebf7b0ea02f8fea540abecc8ed60934c1dddb38
MuskanSharma20 <mhrr2005@gmail.com>
2024-08-18 17:37:48 +0530

    db configurations

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,15 +1,130 @@
 "use server"
 
-import { scrapeAmazonProduct } from "../scraper";
+import { revalidatePath } from "next/cache";
+import { scrapeAmazonProduct } from "../scraper"; //importing scraped data from index.ts which is in scraper
+import { connectToDb } from "../mongoose";
+import Product from "../models/product.model";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types/type";
+// import { generateEmailBody, sendEmail } from "../nodemailer";
+
+
 
 export async function scrapeAndStoreProduct(productUrl:string){
 
     if(!productUrl) return;
 
     try {
+connectToDb();
         const scrapedProduct =await scrapeAmazonProduct(productUrl);
-        
-    } catch (error:any ) {
-        throw new Error(`Failed to create / update product :${error.message}` )
-    }
+
+        if(!scrapedProduct) return;
+
+        let product=scrapedProduct;  // Initializes product with the scraped data. Checks if the product already exists in the database using the product URL.
+  
+        const existingProduct=await Product.findOne({url:scrapedProduct.url});
+
+        //updating price history
+        if(existingProduct) {  
+            const updatedPriceHistory: any = [
+              ...existingProduct.priceHistory,     
+              { price: scrapedProduct.currentPrice }
+            ]
+      
+            product = {
+              ...scrapedProduct,
+              priceHistory: updatedPriceHistory,
+              lowestPrice: getLowestPrice(updatedPriceHistory),
+              highestPrice: getHighestPrice(updatedPriceHistory),
+              averagePrice: getAveragePrice(updatedPriceHistory),
+            }
+          }
+
+          const newProduct = await Product.findOneAndUpdate(
+            { url: scrapedProduct.url },
+            product,
+            { upsert: true, new: true }
+          );
+      
+
+        //   After updating or creating the product, the revalidatePath function is called to refresh the specific product page, ensuring that the latest data is displayed.
+
+
+          revalidatePath(`/products/${newProduct._id}`);
+
+        } catch (error: any) {
+          throw new Error(`Failed to create/update product: ${error.message}`)
+        }
 }
+
+
+// getting product id
+export async function getProductById(productId: string) {
+    try {
+      connectToDb();
+  
+      const product = await Product.findOne({ _id: productId });
+  
+      if(!product) return null;
+  
+      return product;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  // getting all products
+  export async function getAllProducts() {
+    try {
+      connectToDb();
+  
+      const products = await Product.find();
+  
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  //getting similar products
+  export async function getSimilarProducts(productId: string) {
+    try {
+      connectToDb();
+  
+      const currentProduct = await Product.findById(productId);
+  
+      if(!currentProduct) return null;
+  
+      const similarProducts = await Product.find({
+        _id: { $ne: productId },
+      }).limit(3);
+  
+      return similarProducts;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+
+  // export async function addUserEmailToProduct(productId: string, userEmail: string) {
+  //   try {
+  //     const product = await Product.findById(productId);
+  
+  //     if(!product) return;
+  
+  //     const userExists = product.users.some((user: User) => user.email === userEmail);
+  
+  //     if(!userExists) {
+  //       product.users.push({ email: userEmail });
+  
+  //       await product.save();
+  
+  //       // const emailContent = await generateEmailBody(product, "WELCOME");
+  
+  //       // await sendEmail(emailContent, [userEmail]);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { extractPrice } from '../utils';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
 export async function scrapeAmazonProduct(url:string) {
     
@@ -27,7 +27,9 @@ export async function scrapeAmazonProduct(url:string) {
     //fetching product page
     const response = await axios.get(url,options);
    const $ =cheerio.load(response.data);
+
    const title=$('#productTitle').text().trim();
+
    const currentPrice=extractPrice(
       $('.priceToPay span.a-price-whole'),
       $('a.size.base.a-color-price'),
@@ -42,10 +44,40 @@ export async function scrapeAmazonProduct(url:string) {
       $('#priceblock_dealprice'),
       $('.a-size-base.a-color-price')
    );
-   console.log({title});
-        console.log(currentPrice);
-        console.log(originalPrice);
 
+   const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+   const images = 
+   $('#imgBlkFront').attr('data-a-dynamic-image') || 
+   $('#landingImage').attr('data-a-dynamic-image') ||
+   '{}'
+
+   const imageUrls = Object.keys(JSON.parse(images));
+
+   const currency = extractCurrency($('.a-price-symbol'))
+   const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+   const description = extractDescription($)
+
+
+   const data = {
+      url,
+      currency: currency,
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: 'category',
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice:Number(currentPrice)||Number(originalPrice),
+      highestPrice:Number(originalPrice)||Number(currentPrice),
+      average:Number(currentPrice)||Number(originalPrice)
+    }
+
+
+    return data;
  } catch (error:any) {
     throw new Error(`Failed to scrape product:${error.message}`)
  }
